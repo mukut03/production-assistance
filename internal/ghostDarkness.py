4bfc2d182a16bc29c68ee179e5ac18abe60650f7
mukut03 <ilovecars252003mukut@gmail.com>
2024-03-14 19:46:55 -0500

    updated requirements.txt

---
+++ internal/ghostDarkness.py
@@ -0,0 +1,25 @@
+sum_ = "In 1898, Lt. Colonel John Henry Patterson is hired to construct a railway bridge in Tsavo, Kenya. However, the project is hindered by a pair of man-eating lions that target the workers. After initial attempts to kill the lions fail, famous hunter Charles Remington is called in to help. Despite their efforts, the lions continue to terrorize the camp, killing Remington in the process. Patterson eventually kills both lions, the bridge is completed, and he returns to his family."
+num_char = 9
+num_loc = 4
+num_prop = 5
+char_sum = {'Charles Remington': 'A famous hunter brought in to help deal with the lions.',
+            'Colonel John Henry Patterson': 'An engineer tasked with building the bridge, who takes on the responsibility of dealing with the lions.',
+            'Samuel': 'Kenyan foreman, provides local knowledge and assists Patterson and Remington.',
+            'Angus Starling': 'British supervisor who initially guides Patterson at the site.',
+            'David Hawthorne': 'A doctor who informs Patterson of the lion attacks.',
+            'Sir Robert Beaumont': 'The primary financier of the railway project, who pressures Patterson to complete the project on time.',
+            'Helena Patterson': "Patterson's wife, who is awaiting his return in England.",
+            'Abdullah': 'A worker who leads a departure of laborers when the lion attacks become too much.',
+            'Mahina': "The construction foreman who is one of the lions' victims."
+}
+
+loc_sum = {'Tsavo Railway Site': 'The main setting where the bridge for the railway is to be constructed.',
+           'Worker Camps': 'The areas where the laborers live, which are attacked by the lions.',
+           "Patterson's Home in England": "Mentioned as where Patterson's wife and newborn child are.",
+           'Hospital Tent': 'A site set up by Remington for the sick and injured, which later becomes a scene of one of the lion attacks.'}
+
+prop_sum = {"Construction Materials and Tools": "Items used for the building of the bridge, also shown during the construction scenes.",
+            "Firearms and Hunting Equipment": "Used by Patterson, Remington, and others in their attempts to hunt down the lions.",
+            "Thorn Fences": "Constructed around the tents in an effort to keep the lions out.",
+            "Hospital Tent": "A structure that serves as a medical facility for the workers, but also as a site of conflict with the lions.",
+            "Traps and Bait": "Including a stand for Patterson to use while hunting and the animal parts used to lure the lions."}
--- internal/langchain_/pormpts.py
+++ internal/langchain_/pormpts.py
@@ -1,17 +1,45 @@
 from langchain.output_parsers import ResponseSchema
 from langchain.output_parsers import StructuredOutputParser
+from langchain.prompts import PromptTemplate
 
+number_of_chars_schema = ResponseSchema(name="number_of_chars",
+                                        description="Number of unique characters")
+number_of_props_schema = ResponseSchema(name="number_of_props",
+                                        description="Number of unique props")
+number_of_locations_schema = ResponseSchema(name="number_of_locations",
+                                            description="Number of unique locations")
+list_of_chars_schema = ResponseSchema(name="list_of_chars",
+                                      description="List of unique characters")
+list_of_props_schema = ResponseSchema(name="list_of_props",
+                                      description="List of unique props")
+list_of_locations_schema = ResponseSchema(name="list_of_locations",
+                                          description="List of unique locations")
+type_of_chars_schema = ResponseSchema(name="type_of_chars",
+                                      description="Category of chars")
+type_of_locations_schema = ResponseSchema(name="type_of_locations",
+                                          description="Category of chars")
 summary_schema = ResponseSchema(name="summary",
-                                     description="A short summary of the script")
+                                description="A short summary of the script")
 characters_schema = ResponseSchema(name="characters",
-                                     description="A summary of all the characters in the script")
+                                   description="A summary of all the characters in the script")
 props_schema = ResponseSchema(name="props",
-                               description="A summary of all the props in the script")
+                              description="A summary of all the props in the script")
 
+sceneDesc_schema = ResponseSchema(name="sceneDesc",
+                                  description="Descriptions for scenes in the script")
 
-response_schemas = [summary_schema,
+response_schemas = [number_of_chars_schema,
+                    number_of_props_schema,
+                    number_of_locations_schema,
+                    list_of_chars_schema,
+                    list_of_props_schema,
+                    list_of_locations_schema,
+                    type_of_chars_schema,
+                    type_of_locations_schema,
+                    summary_schema,
                     characters_schema,
-                    props_schema]
+                    props_schema,
+                    sceneDesc_schema]
 
 req_parser = StructuredOutputParser.from_response_schemas(response_schemas)
 
@@ -19,11 +47,23 @@ req_template = """\
 From the following pieces of context, extract the following information and format the response as a JSON object with the following keys:
 {context}
 
+number_of_chars: Number of unique characters \
+consider all characters mentioned in the script \
+list_of_chars: Show a comma separated list of all characters \
+type_of_chars: Classify the characters between Human being vs Animal, Man vs Woman, Aged vs Young \
 
-summary: Create a summary of the entire script \
+number_of_props: Number of unique props \
+list_of_props: Show a comma separated list of all props used \
+
+number_of_locations: Number of unique locations \
+list_of_locations: Show a comma separated list of all locations \
+type_of_locations: for each location suggest a nearby site \
+
+summary: Create a summary of the entire script as a story that adds context\
 Include specific information from the script to support your summary \
+If available, this should include the location(s) mentioned in the script
+If available, this should include the time period(s) mentioned in th script
 This should be extracted as a python string \
-
 
 characters: How many characters are in this script?\
 Create a summary including all characters \
@@ -36,9 +76,26 @@ Create a summary of all the props that are used in this script by any/all charac
 Include information about repeated props if possible \
 This should be extracted as a python string \
 
+sceneDesc: Come up with simple visual descriptions for some scenes in the script\
+This could include information such as scene setting, time of day, location, etc \
+Description should include which character is doing what \
+Each description should be a string \
+This should be organized in a dictionary \
+The key should be the name of the scene \
+The value should be a string containing a description for a scene \
+
 Format the output as JSON with the following keys:
+number_of_chars
+number_of_props
+number_of_locations
 summary
 characters
 props
+sceneDesc
 
 """
+
+image_prompt = PromptTemplate(
+    input_variables=["scene_desc", "summary", "characters", "props"],
+    template="Create a prompt for DALL-E image generation that creates a depiction of a movie scene using all the information passed in, scene description: {scene_desc}, overall story summary: {summary}, characters: {characters}, and props:{props} In the final prompt, keep the scene description and the context separate. Put the scene description first, then include all the other information as context.  Explicitly state that there should not be any text in the images generated. Remeber, the maximum length for your response is 4000 characters",
+)
